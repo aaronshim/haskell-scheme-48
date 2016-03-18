340114afd0ed7df4ce03778275b9fc34e4421fbd
Aaron Shim <aaron.k.shim@gmail.com>
2016-03-17 22:39:28 -0400

    Exercise 3

--- hello.hs
+++ hello.hs
@@ -4,7 +4,6 @@ import System.Environment
 
 main :: IO ()
 main = do
-    args <- getArgs
-    let first = read (args !! 0) :: Int
-    let second = read (args !! 1) :: Int
-    putStrLn $ "Result: " ++ show (first + second)
+    putStrLn "What is your name?"
+    response <- getLine
+    putStrLn $ "Hello, " ++ response ++ "!"
